8ff0ac131de6fef5ba9a4de97618c9d0e7ea1804
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-08-26 21:58:29 +0000

    init

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource'
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            three: path.resolve('./node_modules/three'),
+        },
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            /* favicon: './src/assets/favicon/favicon.ico' */
+        }),
+        /* new CopyPlugin({
+            patterns: [
+                { from: "projects", to: "projects" },
+            ],
+        }), */
+        new MiniCssExtractPlugin(),
+        new CssMinimizerPlugin(),
+    ],
+    devServer: {
+        static: path.resolve(__dirname, 'build'),
+        compress: true,
+        port: 3000,
+    },
+    devtool: 'source-map',
+};
