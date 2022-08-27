73f3742412e74acd8aa908e42c31b4dc827d4bbc
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-08-27 03:53:55 +0000

    remove redundant comments

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import {
     MeshBasicMaterial,
     PerspectiveCamera,
     Scene,
-    Vector2,
     WebGLRenderer,
 } from 'three';
 
@@ -14,22 +13,10 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import Stats from 'three/examples/jsm/libs/stats.module';
 import { GUI } from 'dat.gui';
 
-const mouse = new Vector2();
-const target = new Vector2();
-const windowHalf = new Vector2(window.innerWidth / 2, window.innerHeight / 2);
 const contendor = document.body;
 
-// scene
 const scene = new Scene();
 
-// camera controls
-const onMouseMove = (event: { clientX: number; clientY: number; }) => {
-    mouse.x = (event.clientX - windowHalf.x);
-    mouse.y = (event.clientY - windowHalf.x);
-};
-document.addEventListener('mousemove', onMouseMove, false);
-
-// responsiveness
 const onWindowResize = () => {
     camera.aspect = contendor.clientWidth / contendor.clientHeight;
 
@@ -39,10 +26,9 @@ const onWindowResize = () => {
 };
 window.addEventListener('resize', onWindowResize, false);
 
-// camera
 const camera = new PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.8, 1000);
+camera.position.set(0, 0, 4);
 
-// renderer
 const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -59,7 +45,6 @@ const cube = new Mesh(geometry, material);
 scene.add(cube);
 
 //controls.update() must be called after any manual changes to the camera's transform
-camera.position.set(0, 20, 100);
 controls.update();
 
 // creates a canvas on container div
@@ -74,9 +59,7 @@ that controls viewing these debug stuffs.
 =========================================
 */
 let stats: Stats;
-console.log(process.env.DEBUG);
 if (process.env.DEBUG === "TRUE") {
-    //axeshelper
     const axesHelper = new AxesHelper(1000);
     scene.add(axesHelper);
 
@@ -97,9 +80,6 @@ if (process.env.DEBUG === "TRUE") {
 }
 
 const animate = () => {
-    target.x = (1 - mouse.x) * 0.001;
-    target.y = (1 - mouse.y) * 0.001;
-
     controls.update();
     renderer.render(scene, camera);
 
